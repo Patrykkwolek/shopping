80e5b2915486f3aa27809aa4bb2694139fc38b35
Daniel Kwolek <danielkwolek6@gmail.com>
2021-08-12 13:39:48 +0200

    Add meal description

--- src/modules/mainPage/subPages/home/dishes/dish/dishes.tsx
+++ src/modules/mainPage/subPages/home/dishes/dish/dishes.tsx
@@ -19,37 +19,70 @@ function Dish(props: Props) {
 	const { imgName } = props
 
 	let Image
+	let name
+	let timeToPrepare
+	let price
 
 	switch (imgName) {
 		case 'img1':
 			Image = img1
+			name = 'Sałatka pomidorowa z cukinią i fetą'
+			timeToPrepare = '10-20 min'
+			price = '25,99 zł'
 			break
 		case 'img2':
 			Image = img2
+			name = 'Jajko sadzonę na pomidorach'
+			timeToPrepare = '15-20 min'
+			price = '14,99 zł'
 			break
 		case 'img3':
 			Image = img3
+			name = 'Zupa minestrone'
+			timeToPrepare = '30-40 min'
+			price = '29,99 zł'
 			break
 		case 'img4':
 			Image = img4
+			name = 'Zapiekanki z cukini'
+			timeToPrepare = '25-30 min'
+			price = '14,99 zł'
 			break
 		case 'img5':
 			Image = img5
+			name = 'Suflaki'
+			timeToPrepare = '25-35 min'
+			price = '49,99 zł'
 			break
 		case 'img6':
 			Image = img6
+			name = 'Makaron z krewetkami'
+			timeToPrepare = '20-25 min'
+			price = '49,99 zł'
 			break
 		case 'img7':
 			Image = img7
+			name = 'Schabowe z kapustą'
+			timeToPrepare = '30-40 min'
+			price = '29,99 zł'
 			break
 		case 'img8':
 			Image = img8
+			name = 'Sałatka warzywna z kaszą bulgur, fetą i jarmużem'
+			timeToPrepare = '15-20 min'
+			price = '19,99 zł'
 			break
 		case 'img9':
 			Image = img9
+			name = 'Łosoś ze szparagami'
+			timeToPrepare = '30-50 min'
+			price = '59,99 zł'
 			break
 		case 'img10':
 			Image = img10
+			name = 'Sałatka z makaronem i cukinią'
+			timeToPrepare = '20-30 min'
+			price = '29,99 zł'
 			break
 		default:
 			break
@@ -58,6 +91,11 @@ function Dish(props: Props) {
 	return (
 		<div className='dish'>
 			<img src={Image} alt='' className='' width='100%' />
+			<div className='dish__name'>{name}</div>
+			<div className='dish__items'>
+				<span className='dish__time-item'>{timeToPrepare}</span>
+				<span className='dish__price-item'>{price}</span>
+			</div>
 		</div>
 	)
 }
